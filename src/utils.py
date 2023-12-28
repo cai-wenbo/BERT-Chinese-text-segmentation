434d071418935ce1f418f499fc23f18b51d4df96
cai-wenbo <508819545@qq.com>
2023-12-28 05:31:32 +0000

    added utils.py

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+from transformers import BertTokenizer
+
+
+
+'''
+input: a string
+output: a list to feed to model
+'''
+class TextCoder():
+    def __init__(self, model_used):
+        self.tokenizer = BertTokenizer.from_pretrained(model_used)
+
+
+    def __call__(self, input_text):
+        encoding = self.tokenizer(input_text, add_special_tokens=False)
+
+        text_tensor  = torch.tensor(encoding['input_ids']  , dtype = torch.long)
+        mask_tensor  = torch.tensor(encoding['attention_mask']  , dtype = torch.long)
+
+
+        return text_tensor, mask_tensor
